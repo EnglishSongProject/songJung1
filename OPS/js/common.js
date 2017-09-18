ee992142981ff8b9511a0d1faceb7d15e9a7c8ef
wonhui.ryu <dbdnjsgml9@naver.com>
2017-09-18 14:57:16 +0900

    no message

--- OPS/js/common.js
+++ OPS/js/common.js
@@ -56,7 +56,101 @@ function playAudio(path, btnId) {
         }
     }
 }
-function textClear(textId){
-    var textId= document.getElementById(textId);
-    textId.value='';
+
+
+//슬라이더 js
+var imageSlide = {
+
+    init : function(){
+
+        var slide_content = document.getElementsByClassName('slider_content');
+        for(var i=0,j=slide_content.length; i<j; i++){
+            var slider_image = slide_content[i].getElementsByClassName('slider_image')[0];
+            var image = slider_image.children;
+            var thumbnail = slide_content[i].getElementsByClassName('slider_thumbnail')[0];
+            var thumbnail_list = thumbnail.children;
+            slider_image.firstElementChild.classList.add('on');
+            for(var m=0,n=thumbnail_list.length; m<n; m++){
+                if(m===0) thumbnail_list[m].classList.add('on');
+                thumbnail_list[m].addEventListener('mousedown',imageSlide.showImg,false);
+                image[m].addEventListener('touchstart', imageSlide.swipe.swiper, false);
+                image[m].addEventListener('touchmove', imageSlide.swipe.swiper, false);
+                image[m].addEventListener('touchend',imageSlide.swipe.swiper, false);
+            }
+        }
+    },
+
+
+    showImg  : function(){
+        var content =  this.parentNode.parentNode.parentNode;
+        var index = parseInt(this.getAttribute('data-num'));
+        var slide = content.getElementsByClassName('slider_image')[0];
+        this.parentNode.getElementsByClassName('on')[0].classList.remove('on');
+        this.classList.add('on');
+        slide.getElementsByClassName('on')[0].classList.remove('on');
+        slide.children[index-1].classList.add('on');
+    },
+
+    buttonAction : {
+
+        prevImageShow : function(){
+            console.log(this)
+            var slide = this.getElementsByClassName('slider_image')[0];
+            var thumbnail = this.getElementsByClassName('slider_thumbnail')[0];
+            var index = parseInt(thumbnail.getElementsByClassName('on')[0].getAttribute('num'));
+            if(index === 1) {
+                return false;
+            }else{
+                slide.getElementsByClassName('on')[0].classList.remove('on');
+                slide.children[index-2].classList.add('on');
+                thumbnail.getElementsByClassName('on')[0].classList.remove('on');
+                thumbnail.children[index-2].classList.add('on');
+            }
+
+
+        },
+
+        nextImageShow : function(){
+            var slide = this.getElementsByClassName('slider_image')[0];
+            var thumbnail = this.getElementsByClassName('slider_thumbnail')[0];
+            var index = parseInt(thumbnail.getElementsByClassName('on')[0].getAttribute('num'));
+
+            if(index === slide.children.length) {
+                return false;
+            }
+            else {
+                slide.getElementsByClassName('on')[0].classList.remove('on');
+                slide.children[index].classList.add('on');
+                thumbnail.getElementsByClassName('on')[0].classList.remove('on');
+                thumbnail.children[index].classList.add('on');
+            }
+        }
+    },
+    swipe : {
+        touch : null,
+        start_x : null,
+        start_y : null,
+        end_x : null,
+        end_y : null,
+        move : false,
+        swiper : function(event){
+            if (typeof event !== 'undefined') {
+                if (typeof event.touches !== 'undefined') {
+                    imageSlide.swipe.touch = event.touches[0];
+                    if(event.type === 'touchstart'){
+                        imageSlide.swipe.start_x = Math.floor(imageSlide.swipe.touch.pageX);
+                        imageSlide.swipe.start_y = Math.floor(imageSlide.swipe.touch.pageY);
+                        imageSlide.swipe.move = false;
+                    }else if(event.type === 'touchmove'){
+                        imageSlide.swipe.move = true;
+                        imageSlide.swipe.end_x = Math.floor(imageSlide.swipe.touch.pageX);
+                        imageSlide.swipe.end_y = Math.floor(imageSlide.swipe.touch.pageY);
+                    }else if(event.type === 'touchend'){
+                        if(imageSlide.swipe.move) imageSlide.swipe.start_x - imageSlide.swipe.end_x > 0 ? imageSlide.buttonAction.prevImageShow.call(this.parentNode.parentNode.parentNode.parentNode) : imageSlide.buttonAction.nextImageShow.call(this.parentNode.parentNode.parentNode.parentNode)
+
+                    }
+                }
+            }
+        }
+    }
 }
