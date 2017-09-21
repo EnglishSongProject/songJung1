ac2d5a736e05dda1930a28d91c7541a80035e86a
wonhui.ryu <dbdnjsgml9@naver.com>
2017-09-21 13:49:09 +0900

    no message

--- OPS/js/13.js
+++ OPS/js/13.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
     imageSlide.init();
+
+    $("#p13_self_check_pen_1_1, #p13_self_check_pen_1_2, #p13_self_check_pen_1_3, #p13_self_check_pen_1_4, #p13_self_check_pen_1_5").on("click",function(){
+
+
+
+    });
+
+    $("#p13_self_check_pen_2_1, #p13_self_check_pen_2_2, #p13_self_check_pen_2_3, #p13_self_check_pen_2_4, #p13_self_check_pen_2_5").on("click",function(){
+
+
+
+    });
 });
