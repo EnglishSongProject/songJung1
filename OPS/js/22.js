2ea228280429a734550bc2310232515dfda7b0c4
DESKTOP-G992LNM\sr01-04 <kakaki12345@nate.com>
2017-09-22 13:53:39 +0900

    no message

--- OPS/js/22.js
+++ OPS/js/22.js
@@ -1,8 +1,7 @@
-/**
- * Created by sr01-02 on 2017-09-21.
- */
-$(document).ready(function () {
-   $('.btn_tip').on('click', function () {
-       $('.transfer').toggle();
-   });
-});
+
+ // JavaScript Document
+ $(document).ready(function(){
+     $(".btn_tip").on("click", function(){
+         $(".transfer").toggle();
+     });
+ });
