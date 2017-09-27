08b59ae00131c214dd84a234b266884c95e6a69f
wonhui.ryu <dbdnjsgml9@naver.com>
2017-09-27 15:06:41 +0900

    no message

--- OPS/js/21.js
+++ OPS/js/21.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
     imageSlide.init();
+
+    $('#p21_text_pop_btn').on('click', function () {
+       $('.p21_text_pop_txt').css('display', 'block');
+    });
+
+    $('.close_icon').on('click', function () {
+        textClear('ls_4_t_area');
+        $('.p21_text_pop_txt').css('display', 'none');
+    });
+
 });
