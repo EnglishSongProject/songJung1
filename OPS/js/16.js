361f4d7b5f21dc6c85a243e395b1e4fdba9048e0
siyeon.bok <siyeon0821@nate.com>
2017-09-27 18:40:54 +0900

    no message

---
+++ OPS/js/16.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+$(document).ready(function(){
+    $(".btn_trans").on("click", function(){
+        $(".correct").toggle();
+    });
+    $('.img-popup-none-confirm1').on('click',function () {
+        $('.img-popup-none-confirm1').toggle();
+    });
+    $('.btn_play').on('click',function () {
+        $('.img-popup-none-confirm1').toggle();
+    });
+
+    $(".btn_write").on("click",function(){
+        $("#a_text").toggle();
+        $("#ap_aws").hide();
+    });
+
+    $(".btn_wp").on("click",function(){
+        var idx = $(this).index();
+        $('.wpopup').eq(idx).toggle().siblings('.wpopup').hide();
+    });
+
+});
