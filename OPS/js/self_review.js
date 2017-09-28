a728f3e7555a8fcf56e3ce3417c798044f3a7e22
minseok.kang <hawk_z@nate.com>
2017-09-28 10:00:40 +0900

    no message

--- OPS/js/self_review.js
+++ OPS/js/self_review.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
     imageSlide.init();
+
+    $(".btn_selfcheck").on("click",function() {
+        $(".popup_self_review").fadeToggle(200);
+    })
+
+    $(".popup_self_review .btn_close").on("click",function() {
+        $(".popup_self_review").hide();
+    })
 });
