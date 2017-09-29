edd6fc057f5541dc276f6d2486ea629ecf2e52a5
siyeon.bok <siyeon0821@nate.com>
2017-09-29 11:18:16 +0900

    no message

--- OPS/js/21.js
+++ OPS/js/21.js
@@ -4,10 +4,9 @@ $(document).ready(function () {
     $('#p21_text_pop_btn').on('click', function () {
        $('.p21_text_pop_txt').css('display', 'block');
     });
-
-    $('.close_icon').on('click', function () {
+	   
+	$('.close_icon').on('click', function () {
         textClear('ls_4_t_area');
         $('.p21_text_pop_txt').css('display', 'none');
     });
-
 });
