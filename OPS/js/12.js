7e54699657d46bd271c4a5fa4bb2df1927c9e69c
wonhui.ryu <dbdnjsgml9@naver.com>
2017-10-03 15:23:02 +0900

    no message

--- OPS/js/12.js
+++ OPS/js/12.js
@@ -1,7 +1,35 @@
 // JavaScript Document
 var pageNum = 12; //video.js에서 페이지 이미지 경로로 활용됨
-var playMode = 'role';
+var playMode = 'normal';
+var roleNum = 0;
+var progLimit = [0, 200];
+var progBallLeft = 340;
+var mediaInfo = [
 
+    {
+        //하나의 동영상
+
+        //동영상 제목
+        title: "",
+
+        folder: [
+            // 동영상 경로
+            './media/video/L01_role_play'
+        ],
+        rolePos:[100, 0, 110], //'선생님:'의 넓이, 콜론의 x좌표, 대사의 x좌표
+
+        section: [
+        ],
+
+        // 동영상 텍스트 씽크 [순번, 시작시간, 종료시간]
+        sync: [
+        ],
+
+        // 동영상 텍스트 [순번, 사람번호, 영어, 한글]
+        syncText: [
+        ]
+    }
+];
 $(document).ready(function() {
       imageSlide.init();
       $("ul.Tab_area > li").click(function(){
