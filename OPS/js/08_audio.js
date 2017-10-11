27784897d17c6ef0554f2bb6401e74b264def213
wonhui.ryu <dbdnjsgml9@naver.com>
2017-10-11 12:39:29 +0900

    no message

---
+++ OPS/js/08_audio.js
@@ -0,0 +1,29 @@
+/**
+ * Created by sr01-02 on 2017-10-11.
+ */
+var singleAudioInfo = ['./media/audio/L01/008/L01_008_intro_01_k',
+    './media/audio/L01/008/L01_008_intro_01_k2',
+    './media/audio/L01/008/L01_008_intro_02',
+    './media/audio/L01/008/L01_008_intro_03',
+    './media/audio/L01/008/L01_008_Study_Point',
+    './media/audio/L01/008/L01_008_intro_topic',
+    './media/audio/L01/008/L01_008_intro1_02',
+    './media/audio/L01/008/L01_008_intro_functions',
+    './media/audio/L01/008/L01_008_intro1_03',
+    './media/audio/L01/008/L01_008_intro_sp03',
+    './media/audio/L01/008/L01_008_intro_sp04.mp3',
+    './media/audio/L01/008/L01_008_intro1_04.mp3',
+    './media/audio/L01/008/L01_008_intro_sp06.mp3',
+    './media/audio/L01/008/L01_008_intro_sp08.mp3',
+    './media/audio/L01/008/L01_008_intro_forms.mp3',
+    './media/audio/L01/008/L01_008_intro_sp10.mp3',
+    './media/audio/L01/008/L01_008_intro_sp10_2.mp3',
+    './media/audio/L01/008/L01_008_intro_sp11.mp3',
+    './media/audio/L01/008/L01_008_intro_sp11_1.mp3'
+]
+
+var addAudioInfo = ['./media/audio/L01/008/add/L01_008_intro_add_01',
+    './media/audio/L01/008/add/L01_008_intro_add_02',
+    './media/audio/L01/008/add/L01_008_intro_sp07_add',
+    './media/audio/L01/008/add/L01_008_intro_add_03'
+]
