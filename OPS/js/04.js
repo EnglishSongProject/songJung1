13f840b4e4330656ad0fac8e85617fd72ea51219
wonhui.ryu <dbdnjsgml9@naver.com>
2017-10-16 21:51:06 +0900

    no message

--- OPS/js/04.js
+++ OPS/js/04.js
@@ -1,36 +1,12 @@
 /**
  * Created by sr01-02 on 2017-10-15.
  */
-<<<<<<< HEAD
-
 $(document).ready(function () {
 	
     setAudio();
     setPopVideo();
     setShowScriptPopup();
-	
-	function setShowScriptPopup() {
-    var scriptBtnArray = $('.btn_sp');
-    var scriptArray = $('.script');
-    for(var i=0; i<scriptBtnArray.length; i++){
-        $(this).attr('id', 'script_'+i.toString());
-        $(scriptArray[i]).addClass('script_'+i.toString());
-        var topLeft = $(scriptArray[i]).attr('datasrc').split(',');
-        $(scriptBtnArray[i]).on('click', function () {
-            showScriptPopupTopLeft(this, topLeft[0], topLeft[1]);
-        });
-    }
-}
-	
-=======
- 
- $(document).ready(function () {
 
-    setAudio();
-    setPopVideo();
-
-
->>>>>>> 7b759fd0d3500a2bcd3fea6250f1ac18c6126e92
     imageSlide.init();
     $(".btn_play").click(function(){
         imageSlide.closePopup();
@@ -50,19 +26,4 @@ $(document).ready(function () {
     $(".trans3").on("click", function(){
         $(".correct3").toggle();
     });
-<<<<<<< HEAD
-
-    $('.mediabtn_0').on('click',function(){
-        layerAclose();
-        $('.zoom_img_wrap.a').show();
-        mediaInit(0);
-    });
-
-    $('.btn_media_close').on('click',function(){
-        mediaClose(); //video.js
-        AllOff();
-        $('.zoom_img_wrap').hide();
-    });
-=======
->>>>>>> 7b759fd0d3500a2bcd3fea6250f1ac18c6126e92
 });
