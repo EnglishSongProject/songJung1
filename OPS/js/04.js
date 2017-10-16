7b759fd0d3500a2bcd3fea6250f1ac18c6126e92
sujung.jung <egonschxxxx@gmail.com>
2017-10-16 21:09:57 +0900

    no message

--- OPS/js/04.js
+++ OPS/js/04.js
@@ -1,7 +1,30 @@
 /**
  * Created by sr01-02 on 2017-10-15.
  */
-$(function () {
+ 
+ $(document).ready(function () {
+
     setAudio();
     setPopVideo();
+
+
+    imageSlide.init();
+    $(".btn_play").click(function(){
+        imageSlide.closePopup();
+    });
+
+    $(".select_name").on("click",function(e){
+        e.preventDefault();
+        var idx = $(this).index();
+        $(".eg > div").hide();
+        $(".eg > div").eq(idx).show();
+    });
+
+    $(".btn_tip").on("click",function(){
+        $(".tip_con").toggle();
+    });
+
+    $(".trans3").on("click", function(){
+        $(".correct3").toggle();
+    });
 });
