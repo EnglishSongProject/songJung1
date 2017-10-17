6c842e64fdfeee726f86bb828aff13b923af2eff
minseok.kang <hawk_z@nate.com>
2017-10-17 11:56:06 +0900

    no message

--- OPS/js/10.js
+++ OPS/js/10.js
@@ -2,9 +2,31 @@
 $(function () {
     setAudio();
     setPopVideo();
+
+    $(".btn_net").on("click",function(){
+        showWiduPopup(this)
+    });
 });
 
-/*
-$(document).ready(function() {
-    imageSlide.init();
-});*/
+
+function showWiduPopup(target){
+    var $popup = $(".wido_popup");
+
+    //var position = $(target).attr('datasrc').split(',');
+    $popup.show();
+
+    $(".popup .btn_close").on('click',function(){
+        var $this = $(this).parent();
+        $this.hide();
+    });
+
+    /*
+    posX = position[0] || 0;
+    posY = position[1] || 0;
+
+    $popup.css({
+        left:$popup.offset().left - posX,
+        top: $popup.offset().top  - posY
+    });
+    */
+}
